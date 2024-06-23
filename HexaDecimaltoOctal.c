49fd28ee87c6d3cb535a17c66890ba0a21b98e93
Usama Akif Hossain Ador <140923439+UsamaAkifHossainAdor@users.noreply.github.com>
2024-06-23 19:10:03 +0600

    Add files via upload

---
+++ HexaDecimaltoOctal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main () {
+    int number;
+    printf(" Hexa-Decimal number:  ");
+    scanf("%x",&number);
+
+    printf(" Octal number = %o",number);
+    getch ();
+}
---
+++ octaltoHexadecimal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main () {
+    int number;
+    printf(" Octal number:  ");
+    scanf("%o",&number);
+
+    printf(" Hexa-Decimal number = %x",number);
+    getch ();
+}
